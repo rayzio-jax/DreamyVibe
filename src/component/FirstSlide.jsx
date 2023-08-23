1859222a93f48b0329656e7d1b9b09f191af7b05
rayhan <rayhananthaakbar@gmail.com>
2023-08-23 21:17:47 +0700

    update: add files and update file paths

--- src/component/FirstSlide.jsx
+++ src/component/FirstSlide.jsx
@@ -9,7 +9,7 @@ export default function FirstSlide(props) {
 	return (
 		<div ref={props.slide} className="container">
 			<h1>Hei kamu~</h1>
-			<img src="/rayzio-jax/src/assets/hello.gif" alt="" />
+			<img src="./rayzio-jax/assets/hello.gif" alt="" />
 			<div className="downarrow-icon" onClick={props.event}>
 				<ion-icon name="chevron-down-circle"></ion-icon>
 			</div>
