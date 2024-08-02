0238adb69c947e9b1661c458737a4bc2db71a297
rayhan <rayhananthaakbar@gmail.com>
2024-08-02 19:56:33 +0700

    refactor notif to react

--- src/component/Notification.jsx
+++ src/component/Notification.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+import "../scss/notification.scss";
+
 export default function Notification() {
+	const [animationTriggered, setAnimationTriggered] = useState(false);
+
+	useEffect(() => {
+		// Scroll to the top of the page when the component mounts
+		window.scrollTo(0, 0);
+
+		// Set a timer to trigger animation after 10 seconds
+		const timer = setTimeout(() => {
+			setAnimationTriggered(true);
+			console.log("notification appeared");
+		}, 5000);
+
+		// Clean up the timer if the component unmounts
+		return () => clearTimeout(timer);
+	}, []);
+
+	const handleNotificationClick = () => {
+		window.location.reload();
+	};
+
 	return (
-		<div className="wrapper-notif">
-			<button className="notification">
-				<p>Need restart?</p>
+		<div className={`wrapper-notif ${animationTriggered ? "animate" : ""}`}>
+			<button className="notification" onClick={handleNotificationClick}>
+				<p>Restart?</p>
 				<div className="check-icon">
 					<ion-icon name="checkmark-circle"></ion-icon>
 				</div>
