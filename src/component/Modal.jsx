8059a1158a45dcef7ec0e69e9591c640706431bc
rayhan <rayhananthaakbar@gmail.com>
2024-08-02 19:56:48 +0700

    refactor modal to react

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,14 +1,28 @@
 import "../scss/modal.scss";
+import PropTypes from "prop-types";
 
-export default function Modal() {
-	const text = "LOVE U SAYANG <3";
+Modal.propTypes = {
+	onClose: PropTypes.func.isRequired,
+	modalVisible: PropTypes.bool.isRequired,
+};
+
+export default function Modal({ onClose, modalVisible }) {
 	return (
-		<div className="modal-bg">
-			<div className="modal">
+		<div className={`modal-bg ${modalVisible ? "show" : "hide"}`}>
+			<div className={`modal ${modalVisible ? "show" : "hide"}`}>
 				<div className="content">
-					<img className="yey-img" src="yey.gif" alt="" />
-					<p>{text}</p>
-					<button className="closeBtn">X</button>
+					<img
+						className="yey-img"
+						src="https://i.ibb.co.com/CwL2NYt/yey.gif"
+						alt="yay-love-you"
+					/>
+					<p>
+						YAYYY <br />
+						LOVE UU MWAHH ♡
+					</p>
+					<button className="closeBtn" onClick={onClose}>
+						X
+					</button>
 				</div>
 			</div>
 		</div>
