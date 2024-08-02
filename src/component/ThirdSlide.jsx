4dfc327ac4f80618d0afed3b8dbcf184286af1e7
rayhan <rayhananthaakbar@gmail.com>
2024-08-02 19:55:45 +0700

    refactor third slide

--- src/component/ThirdSlide.jsx
+++ src/component/ThirdSlide.jsx
@@ -3,18 +3,27 @@ import PropTypes from "prop-types";
 import "../scss/slides.scss";
 
 ThirdSlide.propTypes = {
-	slide: PropTypes.any,
-	event: PropTypes.any,
+	slide: PropTypes.shape({
+		current: PropTypes.instanceOf(Element),
+	}),
+	event: PropTypes.func, // Updated to reflect that `event` is a function
 };
 
-export default function ThirdSlide(props) {
+export default function ThirdSlide({ slide, event }) {
 	return (
-		<div ref={props.slide} className="container">
+		<div ref={slide} className="container">
 			<div className="wrapper">
-				<h1>Kamu wangy banget hehe~</h1>
-				<img className="hanako-img" src="smile.gif" alt="" />
+				<h1 className="definition-you">
+					You are gorgeus, pretty, cute
+					<br />I love everything about you ˚ʚ♡ɞ˚
+				</h1>
+				<img
+					className="hanako-img"
+					src="https://i.ibb.co.com/wpRFNRc/smile.gif"
+					alt="you-look-gorgeus"
+				/>
 			</div>
-			<div className="downarrow-icon" onClick={props.event}>
+			<div className="downarrow-icon" onClick={event}>
 				<ion-icon name="chevron-down-circle"></ion-icon>
 			</div>
 		</div>
