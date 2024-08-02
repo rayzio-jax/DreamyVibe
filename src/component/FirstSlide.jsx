2eb4c8828141d47620c5ee651bd6e1e222c538d5
rayhan <rayhananthaakbar@gmail.com>
2024-08-02 19:55:20 +0700

    refactor first slide

--- src/component/FirstSlide.jsx
+++ src/component/FirstSlide.jsx
@@ -3,18 +3,20 @@ import PropTypes from "prop-types";
 import "../scss/slides.scss";
 
 FirstSlide.propTypes = {
-	slide: PropTypes.any,
-	event: PropTypes.any,
+	slide: PropTypes.shape({
+		current: PropTypes.instanceOf(Element),
+	}),
+	event: PropTypes.func, // Updated to reflect that `event` is a function
 };
 
-export default function FirstSlide(props) {
+export default function FirstSlide({ slide, event }) {
 	return (
-		<div ref={props.slide} className="container">
+		<div ref={slide} className="container">
 			<div className="wrapper">
-				<h1>Hei kamu~</h1>
-				<img src="hello.gif" alt="" />
+				<h1>Hey you</h1>
+				<img src="https://i.ibb.co.com/sQn8wv5/hello.gif" alt="hello-there" />
 			</div>
-			<div className="downarrow-icon" onClick={props.event}>
+			<div className="downarrow-icon" onClick={event}>
 				<ion-icon name="chevron-down-circle"></ion-icon>
 			</div>
 		</div>
