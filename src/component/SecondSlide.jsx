dc960be824954b02362c52c5710e38befbfd6534
rayhan <rayhananthaakbar@gmail.com>
2024-08-02 19:55:29 +0700

    refactor second slide

--- src/component/SecondSlide.jsx
+++ src/component/SecondSlide.jsx
@@ -3,18 +3,20 @@ import PropTypes from "prop-types";
 import "../scss/slides.scss";
 
 SecondSlide.propTypes = {
-	slide: PropTypes.any,
-	event: PropTypes.any,
+	slide: PropTypes.shape({
+		current: PropTypes.instanceOf(Element),
+	}),
+	event: PropTypes.func, // Updated to reflect that `event` is a function
 };
 
-export default function SecondSlide(props) {
+export default function SecondSlide({ slide, event }) {
 	return (
-		<div ref={props.slide} className="container">
+		<div ref={slide} className="container">
 			<div className="wrapper">
-				<h1>Tau ga sih?</h1>
-				<img src="ya_know.gif" alt="" />
+				<h1>Did you know this?</h1>
+				<img src="https://i.ibb.co.com/9NLPS50/ya-know.gif" alt="ya-know" />
 			</div>
-			<div className="downarrow-icon" onClick={props.event}>
+			<div className="downarrow-icon" onClick={event}>
 				<ion-icon name="chevron-down-circle"></ion-icon>
 			</div>
 		</div>
